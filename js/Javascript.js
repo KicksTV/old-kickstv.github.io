d750c325daa32c363d4c12e0e697892d4c328987
unknown <kicksmedia1@gmail.com>
2015-09-06 16:37:24 +0100

    clickable logos, hover on navbar

--- js/Javascript.js
+++ js/Javascript.js
@@ -3,8 +3,14 @@ $('.handle').on('click', function(){
 $('nav ul').toggleClass('showing');
 });
 
-function changeBG(el,bg) {
-	var elem = document.getElementById(el);
-	elem.style.transition = "background 2.0s linear 2s"
-	elem.style.background = bg;
+$('.clicklogos').on('click', function(){
+$('h1').toggleClass('showtext');
+});
+
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
 }
