197b032f96cffb8477464cc3c0613cdc67c33498
KicksTV <Kicksmedia1@gmail.com>
2015-12-12 12:14:42 +0000

    Test

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,6 +1,23 @@
+var imageclick = function() {
+	$('.imagecontainer1').click(function() {
+		$('.largeimagecontainer').show(100);
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
 window.setInterval(bgtimer, 10000);
 
 var bgtimer = function() {
 	$('.maincontainer').
 
 };
+$(document).ready(main);
