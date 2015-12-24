5144a11b90cbf8358d3e04f8f708c62bf2434b8c
KicksTV <Kicksmedia1@gmail.com>
2015-12-24 19:29:28 +0000

    Small changes

--- js/Javascript.js
+++ js/Javascript.js
@@ -2,40 +2,23 @@ var imageclick1 = function() {
 	$('.imagecontainer1').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image1');
 	});
-};
-var imageclick2 = function() {
 	$('.imagecontainer2').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image2');
 	});
-};
-var imageclick3 = function() {
 	$('.imagecontainer3').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image3');
 	});
-};
-var imageclick4 = function() {
 	$('.imagecontainer4').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image4');
 	});
-};
-var imageclick5 = function() {
 	$('.imagecontainer5').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image5');
 	});
-};
-var imageclick6 = function() {
 	$('.imagecontainer6').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image6');
 	});
+
 };
 
 
-
-
-
 $(document).ready(imageclick1);
-$(document).ready(imageclick2);
-$(document).ready(imageclick3); 
-$(document).ready(imageclick4); 
-$(document).ready(imageclick5); 
-$(document).ready(imageclick6); 
