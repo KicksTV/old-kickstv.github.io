46590992eaffa07a9404741c88efaf17cbf99c4f
KicksTV <Kicksmedia1@gmail.com>
2015-12-24 19:06:52 +0000

    image gallery, javascript

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,6 +1,31 @@
-var imageclick = function() {
+var imageclick1 = function() {
 	$('.imagecontainer1').click(function() {
-		$('.largeimagecontainer').show(100);
+		$('.large_image_container').show(100).attr('id', 'image1');
+	});
+};
+var imageclick2 = function() {
+	$('.imagecontainer2').click(function() {
+		$('.large_image_container').show(100).attr('id', 'image2');
+	});
+};
+var imageclick3 = function() {
+	$('.imagecontainer3').click(function() {
+		$('.large_image_container').show(100).attr('id', 'image3');
+	});
+};
+var imageclick4 = function() {
+	$('.imagecontainer4').click(function() {
+		$('.large_image_container').show(100).attr('id', 'image4');
+	});
+};
+var imageclick5 = function() {
+	$('.imagecontainer5').click(function() {
+		$('.large_image_container').show(100).attr('id', 'image5');
+	});
+};
+var imageclick6 = function() {
+	$('.imagecontainer6').click(function() {
+		$('.large_image_container').show(100).attr('id', 'image6');
 	});
 };
 
@@ -8,16 +33,9 @@ var imageclick = function() {
 
 
 
-
-
-
-
-
-
-window.setInterval(bgtimer, 10000);
-
-var bgtimer = function() {
-	$('.maincontainer').
-
-};
-$(document).ready(main);
+$(document).ready(imageclick1);
+$(document).ready(imageclick2);
+$(document).ready(imageclick3); 
+$(document).ready(imageclick4); 
+$(document).ready(imageclick5); 
+$(document).ready(imageclick6); 
