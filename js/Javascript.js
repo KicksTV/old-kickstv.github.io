6b7e95d89439b9bc61a9e80f13ae9d834c926894
KicksTV <Kicksmedia1@gmail.com>
2015-12-24 22:56:32 +0000

    Change portfolio page

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,20 +1,20 @@
 var imageclick1 = function() {
-	$('.imagecontainer1').click(function() {
+	$('#image1').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image1');
 	});
-	$('.imagecontainer2').click(function() {
+	$('#image2').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image2');
 	});
-	$('.imagecontainer3').click(function() {
+	$('#image3').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image3');
 	});
-	$('.imagecontainer4').click(function() {
+	$('#image4').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image4');
 	});
-	$('.imagecontainer5').click(function() {
+	$('#image5').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image5');
 	});
-	$('.imagecontainer6').click(function() {
+	$('#image6').click(function() {
 		$('.large_image_container').show(100).attr('id', 'image6');
 	});
 
